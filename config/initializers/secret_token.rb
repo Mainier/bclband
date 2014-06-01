a307770174f0ed51f1a587b30924fcc395ef8dbd
vagrant <vagrant@precise32.(none)>
2014-06-01 02:55:04 +0000

    Creating the static controller and the index.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bclband::Application.config.secret_key_base = '62918769be2b0b2757f35a1f60b5afc4d1f124ceb19e91689f6f8f5ef2bcbf5c8b43ee29bb688c0aef97ccfa1c8edb107b981c47cab5e5fd17c4cc72bf679d40'
